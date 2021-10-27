e30d496ce09151fa3d0b8bb935d41bd0158de80e
Edson Lima <edsoneduardo526@gmail.com>
2021-10-27 15:34:34 -0300

    Adiciona estilo ao menu lateral

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,49 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getUser } from '../services/userAPI';
 import '../styles/Header.css';
+import logo from '../images/logo.png';
+import search from '../images/pesquisar.png';
+import favorite from '../images/favorito.png';
+import profile from '../images/perfil.png';
 
 class Header extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      loading: true,
-      loadComplete: false,
-      nome: '',
-    };
-
-    this.callGetUser = this.callGetUser.bind(this);
-  }
-
-  componentDidMount() {
-    this.callGetUser();
-  }
-
-  callGetUser() {
-    this.setState({
-      loading: true,
-    }, async () => {
-      const userName = await getUser();
-      this.setState({
-        nome: userName.name,
-        loading: false,
-        loadComplete: true,
-      });
-    });
-  }
 
   render() {
-    const { loading, loadComplete, nome } = this.state;
     return (
       <aside data-testid="header-component" className="menu-lateral">
-        { loading === true && <h1>Carregando...</h1> }
-        { loadComplete === true && <h1 data-testid="header-user-name">{ nome }</h1> }
+        <img src={ logo } alt="logo" className="logo" />
         <nav>
-          <Link to="/search" data-testid="link-to-search" className="link-nav">Pesquisar</Link>
-          <Link to="/favorites" data-testid="link-to-favorites" className="link-nav">Favoritas</Link>
-          <Link to="/profile" data-testid="link-to-profile" className="link-nav">Perfil</Link>
+          <Link
+            to="/search"
+            data-testid="link-to-search"
+            className="link-nav">
+              <img src={ search } alt="lupa" className="icon" />
+              Pesquisar
+          </Link>
+          <Link
+          to="/favorites"
+          data-testid="link-to-favorites"
+          className="link-nav">
+            <img src={ favorite } alt="lupa" className="icon" />
+            Favoritas
+          </Link>
+          <Link
+          to="/profile"
+          data-testid="link-to-profile"
+          className="link-nav">
+            <img src={ profile } alt="lupa" className="icon" />
+            Perfil
+            </Link>
         </nav>
+        <div className="copy">&copy; <h3>Edson Lima</h3></div>
       </aside>
     );
   }
